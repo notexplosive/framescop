4665e61f7cf908c79ebda711e144ef1b6bfc8124
notexplosive <thenotexplosive@gmail.com>
2019-04-22 21:06:35 -0700

    Fixed null-frame crash

--- film.lua
+++ film.lua
@@ -1,9 +1,9 @@
 -- We're calling this "Film" because it's a series of static images played back
 -- to make it look like it's a contiguous video.
 
-require('stringutil')
-local Keyframe = require('keyframe')
-local Timeline = require('timeline')
+require("stringutil")
+local Keyframe = require("keyframe")
+local Timeline = require("timeline")
 
 -- The binary most likely isn't at original framerate (30), so we scale up the "current frame" we're on
 local realFPS = 30
@@ -15,16 +15,16 @@ Film.new = function(dirPath)
     local self = {}
     setmetatable(self, Film)
 
-    local split = dirPath:split('/')
+    local split = dirPath:split("/")
     FILE_NAME = split[#split]
-    FileMgr.trackPath = FILE_NAME .. '.tsv'
+    FileMgr.trackPath = FILE_NAME .. ".tsv"
     updateWindowTitle()
 
     self.playhead = 1
- 
+
     self.title = dirPath
     self.fps = 15
-    self.totalFrames = #love.filesystem.getDirectoryItems(dirPath)-1
+    self.totalFrames = #love.filesystem.getDirectoryItems(dirPath) - 1
     self.path = dirPath
     self.warning = false
     self.warningTimer = 0
@@ -37,7 +37,7 @@ Film.new = function(dirPath)
     self.timeline = Timeline.new(self)
 
     self.data = {}
-    self:h_loadAt(1,60)
+    self:h_loadAt(1, 60)
 
     FileMgr.init(self)
     FileMgr.load(self)
@@ -45,7 +45,7 @@ Film.new = function(dirPath)
     return self
 end
 
-Film.update = function(self,dt)
+Film.update = function(self, dt)
     -- This ticks up every frame but gets reset when a key is pressed
     self.idleTimer = self.idleTimer + dt
     self.preloading = false
@@ -60,14 +60,14 @@ Film.update = function(self,dt)
     end
 
     if not self.data[self.playhead + 10] then
-        printst('Loading...')
+        printst("Loading...")
         self.warning = true
         self.warningTimer = self.warningTimer + dt
 
         -- Warning enables the red border. If it's been red for 1 second, just load the next
         -- chunk, we warned them so they will expect the lag.
         if self.warningTimer > .25 then
-            self:h_loadAt(self:h_nextUnloadedFromPlayhead(),15)
+            self:h_loadAt(self:h_nextUnloadedFromPlayhead(), 15)
             self.warningTimer = 0
         end
     else
@@ -76,7 +76,7 @@ Film.update = function(self,dt)
     end
 
     if self.idleTimer > 1 then
-        if self:h_loadAt(self:h_nextUnloadedFromPlayhead(),15) then
+        if self:h_loadAt(self:h_nextUnloadedFromPlayhead(), 15) then
             self.preloading = true
         end
     end
@@ -88,7 +88,7 @@ Film.update = function(self,dt)
         if self.playhead < backFrames then
             backFrames = self.playhead
         end
-        self:h_loadAt(self.playhead-backFrames,15)
+        self:h_loadAt(self.playhead - backFrames, 15)
     end
 
     -- TODO: This could be a lot smarter.
@@ -101,58 +101,59 @@ end
 
 Film.draw = function(self)
     local frame = self:getFrameImage(self.playhead)
-    local scalex = love.graphics.getWidth() / frame:getWidth()
-    local scaley = love.graphics.getHeight() / frame:getHeight()
-    love.graphics.draw(frame,0,0,0,scalex,scaley)
-    
-    self.timeline:draw()
+    if frame then
+        local scalex = love.graphics.getWidth() / frame:getWidth()
+        local scaley = love.graphics.getHeight() / frame:getHeight()
+        love.graphics.draw(frame, 0, 0, 0, scalex, scaley)
+
+        self.timeline:draw()
+    end
 end
 
-Film.getFrameImage = function(self,index)
+Film.getFrameImage = function(self, index)
     if self.data[index] then
         return self.data[index]
     end
-    
-    self:h_loadAt(index,14)
-    return self.data[index]
+
+    if self:h_loadAt(index, 14) then
+        return self.data[index]
+    end
 end
 
-Film.movePlayheadTo = function(self,index)
+Film.movePlayheadTo = function(self, index)
     if index > 0 and index < self.totalFrames then
         self.playhead = index
     end
 end
 
 Film.status = function(self)
-    return 'time: ' .. self:timeString() .. '\t'
-    .. self.framesInMemory .. ' images in memory' .. '\t'
+    return "time: " .. self:timeString() .. "\t" .. self.framesInMemory .. " images in memory" .. "\t"
 end
 
 -- TODO: this conversion doesn't quite work right if the framerate isn't 15
 -- This isn't a huge problem because all of the binaries will be 15 fps
-Film.timeString = function(self,x)
-    if x == nil then 
+Film.timeString = function(self, x)
+    if x == nil then
         x = self.playhead
     end
-    local video_frame = (x-1) * (realFPS / self.fps)
-    local seconds = math.floor(video_frame/realFPS)
-    return string.format("%02d",math.floor(seconds/60)) .. ':'
-    .. string.format("%02d",seconds%60) .. ';' 
-    .. string.format("%02d",video_frame%realFPS)
+    local video_frame = (x - 1) * (realFPS / self.fps)
+    local seconds = math.floor(video_frame / realFPS)
+    return string.format("%02d", math.floor(seconds / 60)) ..
+        ":" .. string.format("%02d", seconds % 60) .. ";" .. string.format("%02d", video_frame % realFPS)
 end
 
-Film.timeStringToFrames = function(self,timeString)
-    if timeString == nil then 
+Film.timeStringToFrames = function(self, timeString)
+    if timeString == nil then
         timeString = self:timeString()
     end
 
-    local tsSplitOnColon = timeString:split(':')
+    local tsSplitOnColon = timeString:split(":")
     local minutes = tsSplitOnColon[1]
-    local seconds = tsSplitOnColon[2]:split(';')[1] + minutes * 60
-    local video_frame = tonumber(timeString:split(';')[2]) / 2
-    
+    local seconds = tsSplitOnColon[2]:split(";")[1] + minutes * 60
+    local video_frame = tonumber(timeString:split(";")[2]) / 2
+
     local frames = seconds * self.fps + video_frame + 1
-    print('read ' .. timeString .. ' as ' .. frames)
+    print("read " .. timeString .. " as " .. frames)
     return frames
 end
 
@@ -161,14 +162,13 @@ Film.getTrackPath = function(self)
 end
 
 Film.getFullTrackPath = function(self)
-    return love.filesystem.getAppdataDirectory() .. '/' .. self:getTrackPath()
+    return love.filesystem.getAppdataDirectory() .. "/" .. self:getTrackPath()
 end
 
 --- HELPER FUNCTIONS BELOW THIS POINT ---
 
-
 -- Helper function to keep the constructor looking clean
-Film.h_loadAt = function(self,location,size)
+Film.h_loadAt = function(self, location, size)
     if location < 1 then
         location = 1
     end
@@ -179,18 +179,18 @@ Film.h_loadAt = function(self,location,size)
 
     local loadedSomething = false
 
-    for i=location,location + size do
+    for i = location, location + size do
         if not self.data[i] then
             self.framesInMemory = self.framesInMemory + 1
-            self.data[i] = love.graphics.newImage(self.path .. '/' .. i .. '.png')
+            self.data[i] = love.graphics.newImage(self.path .. "/" .. i .. ".png")
             loadedSomething = true
-        end 
+        end
     end
 
     return loadedSomething
 end
 
-Film.h_eraseAt = function(self,location,size)
+Film.h_eraseAt = function(self, location, size)
     if location < 1 then
         location = 1
     end
@@ -199,7 +199,7 @@ Film.h_eraseAt = function(self,location,size)
         size = self.totalFrames - location
     end
 
-    for i=location,location+size do
+    for i = location, location + size do
         if self.data[i] then
             -- Delete from table, hand to garbage collector
             self.framesInMemory = self.framesInMemory - 1
@@ -211,7 +211,7 @@ end
 
 Film.h_nextUnloadedFromPlayhead = function(self)
     -- If we've already loaded 240 frames out, we're good.
-    for i=self.playhead,self:h_boundedFromPlayhead(240) do
+    for i = self.playhead, self:h_boundedFromPlayhead(240) do
         if self.data[i] == null then
             self.cachedFrontier = i
             return i
@@ -222,7 +222,7 @@ Film.h_nextUnloadedFromPlayhead = function(self)
 end
 
 Film.h_earliestLoadedFrame = function(self)
-    for i=0,self.playhead do
+    for i = 0, self.playhead do
         if self.data[i] == nil then
             return i
         end
@@ -233,7 +233,7 @@ end
 -- the most nearby stuff
 Film.h_clearData = function(self)
     for i = 0, self.totalFrames do
-        if i < self.playhead-10 or i > self.playhead+240 then
+        if i < self.playhead - 10 or i > self.playhead + 240 then
             if self.data[i] then
                 self.framesInMemory = self.framesInMemory - 1
                 self.data[i]:release()
@@ -245,9 +245,9 @@ end
 
 -- If this offset from playhead is in bounds, return that.
 -- Otherwise return bound we're up against
-Film.h_boundedFromPlayhead = function(self,offset)
+Film.h_boundedFromPlayhead = function(self, offset)
     local val = self.playhead + offset
-    if val < 1 then 
+    if val < 1 then
         val = 1
     end
     if val > self.totalFrames then
